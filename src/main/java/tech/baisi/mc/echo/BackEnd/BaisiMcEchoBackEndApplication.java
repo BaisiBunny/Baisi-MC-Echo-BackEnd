311f8299cab23d2f3d37459d7c118d6aafe45059
BaisiTech <yuanzhixiang@yuanzhixiangdeMacBook-Air.local>
2023-08-19 20:04:50 +0800

    初始化

---
+++ src/main/java/tech/baisi/mc/echo/BackEnd/BaisiMcEchoBackEndApplication.java
@@ -0,0 +1,13 @@
+package tech.baisi.mc.echo.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaisiMcEchoBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaisiMcEchoBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/baisi/mc/echo/BackEnd/BaisiMcEchoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.baisi.mc.echo.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaisiMcEchoBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
